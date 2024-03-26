a5381524f77ee57e7b77199887c7ec2858d9e916
Akshay <akshaygoti23@gmail.com>
2024-03-26 09:22:34 +0530

    26-3-24

--- string.to.integer.js
+++ string.to.integer.js
@@ -1,8 +1,17 @@
 // Input: s = "42"
 // Output: 42
 
-let s = 321;
-console.log(s);
+// let s = 321;
+// console.log(s);
 
-let text = s.toString();
-console.log(text);
+// let text = s.toString();
+// console.log(text);
+
+// let text1 = "   Hello World!";
+// console.log(text1);
+// let text2 = text1.trim();      // Hello world!
+// console.log(text2);
+
+
+let text0 = parseInt('209 akshay');
+console.log(text0);
