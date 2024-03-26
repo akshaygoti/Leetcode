f22042af2a5d4611ab003aad40ccd45c1e2b2423
Akshay <akshaygoti23@gmail.com>
2024-03-26 11:19:08 +0530

    26-3-24

--- lenth.lastworld.js
+++ lenth.lastworld.js
@@ -4,7 +4,42 @@
 // substring
 //  consisting of non-space characters only.
 
-const s = "akshay";
+// const s = "akshay goti";
+// function lengthOfLastWord(s) {
+
+//     if (s.length === 0) {
+//         return 0;
+//     } else {
+//         const words = s.trim().split(' ');   // trim e bane baju ni space ne remove kare
+//         const lengthOfLastWord = words[words.length - 1].length;
+//         return lengthOfLastWord;
+//     }
+// }
+
+// const output = lengthOfLastWord(s);
+// console.log(output);
+
+// 2
+
+// const s = "   akshay goti   ";
+// function lengthOfLastWord(s) {
+
+//     if (s.length === 0) {
+//         return 0;
+//     } else {
+//         const words = s.trim().split(' ');   // trim e bane baju ni space ne remove kare
+//         const lengthOfLastWord = words[words.length - 1].length;
+//         return lengthOfLastWord;
+//     }
+// }
+
+// const output = lengthOfLastWord(s);
+// console.log(output);
+
+
+// 3
+
+const s = "   akshay goti my function all don";
 function lengthOfLastWord(s) {
 
     if (s.length === 0) {
---
+++ sqrt.js
@@ -0,0 +1,5 @@
+{
+  
+    let x = Math.sqrt(8);
+    console.log(x);
+}
